d48859fabd3a26e6f316337f4efb5f72272dee38
Freyr <sky_rider_93@mail.ru>
2017-04-21 12:24:49 +0300

    some advancements

--- src/main.ml
+++ src/main.ml
@@ -1,12 +1,36 @@
 open Batteries
+open Lwt
+open Lwt_react
 
 let pr_time t =
   let tm = Unix.localtime t in
-  Printf.printf "\x1B[8D%02d:%02d:%02d%!"
-                tm.Unix.tm_hour tm.Unix.tm_min tm.Unix.tm_sec
+  Printf.sprintf "%02d:%02d:%02d"
+                 tm.Unix.tm_hour tm.Unix.tm_min tm.Unix.tm_sec
 
-let pr_string =
-  Printf.printf "\x1B[8D%s%!" 
+module type Widget = sig
+  type t = string
+
+  val get_event : unit -> t event * (unit -> unit)
+end
+
+module Time = struct
+  let get_event () =
+    let event, send' = E.create () in
+    let send = fun () -> send' "time" in
+    event, send
+end
+
+module String = struct
+  let get_event () =
+    let event, send' = E.create () in
+    let send = fun () -> send' "string" in
+    event, send
+end 
+    
+type fmt = Time_fmt
+         | String_fmt
+  
+let widgets = [(Time_fmt, 1.); (String_fmt, 3.)]
   
 let rec main ctx =
   let nctx = List.map (fun (f, t, tlim) ->
@@ -17,8 +41,35 @@ let rec main ctx =
   in
   Unix.sleep 1;
   main nctx
+
+type widget = { tm : float
+              ; sender : (string -> unit)
+              ; arg    : string}
   
+let construct w =
+  Lwt_unix.sleep w.tm >>= fun _ ->
+  return @@ w.sender w.arg
+
+let update w thread =
+  match Lwt.state thread with
+  | Lwt.Return _ -> construct w
+  | _            -> thread                
+                                            
 let () =
+  let e1, e1_send = E.create () in
+  let e2, e2_send = E.create () in
+  let wl = [{tm = 1.; sender = e1_send; arg = "Each second"};
+            {tm = 3.; sender = e2_send; arg = "Each 3 secs"}] in
+  let tl = List.map construct wl in 
+  let rec loop tl =
+    Lwt.choose tl >>= fun _ ->
+    List.map2 update wl tl |> loop
+  in
+  let p1 = E.map print_endline e1 in
+  let p2 = E.map print_endline e2 in
+  Lwt_main.run (loop tl)
+  (*
   let f1 = (pr_time % Unix.gettimeofday) in
-  let f2 = (fun () -> pr_string "Hello!") in 
+  let f2 = (fun () -> print_endline "Hello!") in 
   main [(f1,0,0);(f2,1,2)]
+   *)
